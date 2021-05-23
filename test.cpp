b0f578908a9cc6809aaa3b2cdd20bbf59c33e7e2
tpb12 <ptikh@yandex.ru>
2021-05-23 21:03:04 +0300

    Testing non-mfc class

--- test.cpp
+++ test.cpp
@@ -41,16 +41,18 @@
 #define LOG_COMPOSED_TYPE_TO_PACK 0x0007
 #define SOURCE_ID 0x000021
 #define STATUS_REQUEST_MESSAGE_TYPE 0x0003
+#define STATUS_DATA_0 11
+#define STATUS_DATA_1 22
 
-void FillMapWordToPtr(CMapWordToPtr& map)
+void FillMapWordToPtr(CSettings::MapWordToPtr& map)
 {
-    map.RemoveAll();
-    map.SetAt((WORD)0x100, NULL);
-    map.SetAt((WORD)0x200, NULL);
-    map.SetAt((WORD)0x300, NULL);
+    map.clear();
+    map.insert({ (WORD)0x100, NULL });
+    map.insert({ (WORD)0x200, NULL });
+    map.insert({ (WORD)0x300, NULL });
 }
 
-BOOL CheckMapWordToPtr(const CMapWordToPtr& map)
+BOOL CheckMapWordToPtr(const CSettings::MapWordToPtr& map)
 {
     /*POSITION pos = map.GetStartPosition();
     while (pos != NULL)
@@ -85,11 +87,11 @@ public:
         g_Settings.m_iCOMWttc = COM_WTTC;
         g_Settings.m_iCOMRit = COM_RIT;
 
-        CByteArray arTemp;
-        ByteArrayFromString(PREFIX, arTemp, _T(""));
-        g_Settings.m_arPrefix.Copy(arTemp);
-        ByteArrayFromString(OUT_PREFIX, arTemp, _T(""));
-        g_Settings.m_arOutPrefix.Copy(arTemp);
+        //CByteArray arTemp;
+        ByteArrayFromString(PREFIX, g_Settings.m_arPrefix, _T(""));
+        //g_Settings.m_arPrefix.Copy(arTemp);
+        ByteArrayFromString(OUT_PREFIX, g_Settings.m_arOutPrefix, _T(""));
+        //g_Settings.m_arOutPrefix.Copy(arTemp);
 
         g_Settings.m_wComposedType = COMPOSED_TYPE;
         g_Settings.m_wOutputComposedType = OUTPUT_COMPOSED_TYPE;
@@ -113,10 +115,13 @@ public:
         g_Settings.m_MarkNestedMask.m_wSrcMask = MASK_SRC;
 
         //***
+        g_Settings.m_mapMsgTypes.clear();
         CSettings::MESSAGETYPE typeStatus(0x0007, 0x1008);
-        g_Settings.m_mapMsgTypes.SetAt(0x0001, typeStatus);
+        //g_Settings.m_mapMsgTypes.SetAt(0x0001, typeStatus);
+        g_Settings.m_mapMsgTypes.insert({ 0x0001, typeStatus });
         CSettings::MESSAGETYPE typeStatus1(0x0008, 0x1009);
-        g_Settings.m_mapMsgTypes.SetAt(0x0002, typeStatus1);
+        //g_Settings.m_mapMsgTypes.SetAt(0x0002, typeStatus1);
+        g_Settings.m_mapMsgTypes.insert({ 0x0002, typeStatus1 });
 
         //***
         g_Settings.m_StatusHdr = CSettings::MESSAGETYPE(0x0000, 0x20);
@@ -138,6 +143,10 @@ public:
 
         g_Settings.m_wSourceID = SOURCE_ID;
         g_Settings.m_wStatusRequestMessageType = STATUS_REQUEST_MESSAGE_TYPE;
+
+        g_Settings.m_arStatusData.resize(2);
+        g_Settings.m_arStatusData[0] = STATUS_DATA_0;
+        g_Settings.m_arStatusData[1] = STATUS_DATA_1;
     }
     ~TestSettings() { }
     void SetUp() { }
@@ -150,72 +159,76 @@ void t1()
 {
     BOOL res = g_Settings.Save(SET_FILE_NAME);
     EXPECT_TRUE(res);
-    res = g_Settings.Load(SET_FILE_NAME);
+    CSettings local_settings;
+    res = local_settings.Load(SET_FILE_NAME);
     EXPECT_TRUE(res);
-    //g_Settings.MakeStatusMsg();
+    //local_settings.MakeStatusMsg();
 
-    EXPECT_EQ(g_Settings.m_dwPollingPeriod, POLLING_PERIOD);
-    EXPECT_EQ(g_Settings.m_bTestLoopback, TEST_LOOPBACK);
-    EXPECT_EQ(g_Settings.m_bShowSIOMessages, SHOW_SID_MESSAGE);
-    EXPECT_EQ(g_Settings.m_bShowMessageErrors, SHOW_MESSAGE_ERRORS);
-    EXPECT_EQ(g_Settings.m_bShowCOMErrors, SHOW_COM_ERRORS);
+    EXPECT_EQ(local_settings.m_dwPollingPeriod, POLLING_PERIOD);
+    EXPECT_EQ(local_settings.m_bTestLoopback, TEST_LOOPBACK);
+    EXPECT_EQ(local_settings.m_bShowSIOMessages, SHOW_SID_MESSAGE);
+    EXPECT_EQ(local_settings.m_bShowMessageErrors, SHOW_MESSAGE_ERRORS);
+    EXPECT_EQ(local_settings.m_bShowCOMErrors, SHOW_COM_ERRORS);
 
-    ASSERT_STREQ(g_Settings.m_strSettingsReportPath.c_str(), SETTINGS_REPORT_PATH);
+    ASSERT_STREQ(local_settings.m_strSettingsReportPath.c_str(), SETTINGS_REPORT_PATH);
 
-    EXPECT_EQ(g_Settings.m_nBufferSize, BUFFER_SIZE);
-    EXPECT_EQ(g_Settings.m_nIncomingPort, INCOMING_PORT);
-    ASSERT_STREQ(g_Settings.m_strCOMSetup.c_str(), COM_SETUP);
+    EXPECT_EQ(local_settings.m_nBufferSize, BUFFER_SIZE);
+    EXPECT_EQ(local_settings.m_nIncomingPort, INCOMING_PORT);
+    ASSERT_STREQ(local_settings.m_strCOMSetup.c_str(), COM_SETUP);
 
-    ASSERT_EQ(g_Settings.m_iCOMRttc, COM_RTTC);
-    ASSERT_EQ(g_Settings.m_iCOMWttc, COM_WTTC);
-    ASSERT_EQ(g_Settings.m_iCOMRit, COM_RIT);
+    ASSERT_EQ(local_settings.m_iCOMRttc, COM_RTTC);
+    ASSERT_EQ(local_settings.m_iCOMWttc, COM_WTTC);
+    ASSERT_EQ(local_settings.m_iCOMRit, COM_RIT);
 
-    CString strTemp;
-    ByteArrayToString(g_Settings.m_arPrefix.GetData(),
-        (int)g_Settings.m_arPrefix.GetSize(), strTemp, "");
-    ASSERT_STREQ(strTemp, PREFIX);
-    ByteArrayToString(g_Settings.m_arOutPrefix.GetData(),
-        (int)g_Settings.m_arOutPrefix.GetSize(), strTemp, "");
-    ASSERT_STREQ(strTemp, OUT_PREFIX);
+    std::string strTemp;
+    ByteArrayToString(local_settings.m_arPrefix,
+        (int)local_settings.m_arPrefix.size(), strTemp, "");
+    ASSERT_STREQ(strTemp.c_str(), PREFIX);
+    ByteArrayToString(local_settings.m_arOutPrefix,
+        (int)local_settings.m_arOutPrefix.size(), strTemp, "");
+    ASSERT_STREQ(strTemp.c_str(), OUT_PREFIX);
 
-    ASSERT_EQ(g_Settings.m_wComposedType, COMPOSED_TYPE);
-    ASSERT_EQ(g_Settings.m_wOutputComposedType, OUTPUT_COMPOSED_TYPE);
-    ASSERT_EQ(g_Settings.m_wCRC16Init, CRC16_INIT);
-    ASSERT_EQ(g_Settings.m_wCPAddr, CP_ADDR);
-    ASSERT_EQ(g_Settings.m_wPUAddr, PU_ADDR);
+    ASSERT_EQ(local_settings.m_wComposedType, COMPOSED_TYPE);
+    ASSERT_EQ(local_settings.m_wOutputComposedType, OUTPUT_COMPOSED_TYPE);
+    ASSERT_EQ(local_settings.m_wCRC16Init, CRC16_INIT);
+    ASSERT_EQ(local_settings.m_wCPAddr, CP_ADDR);
+    ASSERT_EQ(local_settings.m_wPUAddr, PU_ADDR);
 
-    ASSERT_EQ(g_Settings.m_bUnpackAll, UNPACK_ALL);
-    ASSERT_EQ(g_Settings.m_bMarkAll, MARK_ALL);
-    res = CheckMapWordToPtr(g_Settings.m_mapMsgTypesToMark);
+    ASSERT_EQ(local_settings.m_bUnpackAll, UNPACK_ALL);
+    ASSERT_EQ(local_settings.m_bMarkAll, MARK_ALL);
+    res = CheckMapWordToPtr(local_settings.m_mapMsgTypesToMark);
     ASSERT_EQ(res, TRUE);
 
-    ASSERT_EQ(g_Settings.m_nStatusPeriod, STATUS_PERIOD);
-    ASSERT_EQ(g_Settings.m_iSendStatTO, SEND_STATUS_TO);
+    ASSERT_EQ(local_settings.m_nStatusPeriod, STATUS_PERIOD);
+    ASSERT_EQ(local_settings.m_iSendStatTO, SEND_STATUS_TO);
 
-    ASSERT_EQ(g_Settings.m_MarkComposedMask.m_wDestMask, COMPOSED_MASK_DEST);
-    ASSERT_EQ(g_Settings.m_MarkComposedMask.m_wSrcMask, COMPOSED_MASK_SRC);
-    ASSERT_EQ(g_Settings.m_MarkNestedMask.m_wDestMask, MASK_DEST);
-    ASSERT_EQ(g_Settings.m_MarkNestedMask.m_wSrcMask, MASK_SRC);
+    ASSERT_EQ(local_settings.m_MarkComposedMask.m_wDestMask, COMPOSED_MASK_DEST);
+    ASSERT_EQ(local_settings.m_MarkComposedMask.m_wSrcMask, COMPOSED_MASK_SRC);
+    ASSERT_EQ(local_settings.m_MarkNestedMask.m_wDestMask, MASK_DEST);
+    ASSERT_EQ(local_settings.m_MarkNestedMask.m_wSrcMask, MASK_SRC);
 
-    // POSITION pos = g_Settings.m_mapMsgTypes.GetStartPosition(); //***
-    // g_Settings.m_StatusHdr; //***
-    // g_Settings.m_StatusMsg; //***
+    // POSITION pos = local_settings.m_mapMsgTypes.GetStartPosition(); //***
+    // local_settings.m_StatusHdr; //***
+    // local_settings.m_StatusMsg; //***
 
-    ASSERT_EQ(g_Settings.m_TUType, TU_TYPE);
-    ASSERT_EQ(g_Settings.m_TUSrcMask, TU_SRC_MASK);
-    ASSERT_EQ(g_Settings.m_TUSrcComMsgIndex, TU_SRC_COM_INDEX);
-    ASSERT_EQ(g_Settings.m_TUPrimToSecSrc, TU_PRIM_TO_SEC);
-    ASSERT_EQ(g_Settings.m_TUSecToPrimSrc, TU_SEC_TO_PRIMK);
+    ASSERT_EQ(local_settings.m_TUType, TU_TYPE);
+    ASSERT_EQ(local_settings.m_TUSrcMask, TU_SRC_MASK);
+    ASSERT_EQ(local_settings.m_TUSrcComMsgIndex, TU_SRC_COM_INDEX);
+    ASSERT_EQ(local_settings.m_TUPrimToSecSrc, TU_PRIM_TO_SEC);
+    ASSERT_EQ(local_settings.m_TUSecToPrimSrc, TU_SEC_TO_PRIMK);
 
-    ASSERT_EQ(g_Settings.m_bKeepLog, KEEP_LOG);
-    ASSERT_EQ(g_Settings.m_wLogComposedType, LOG_COMPOSED_TYPE);
-    ASSERT_EQ(g_Settings.m_bLogPackAll, LOG_PACK_ALL);
+    ASSERT_EQ(local_settings.m_bKeepLog, KEEP_LOG);
+    ASSERT_EQ(local_settings.m_wLogComposedType, LOG_COMPOSED_TYPE);
+    ASSERT_EQ(local_settings.m_bLogPackAll, LOG_PACK_ALL);
 
-    ASSERT_EQ(g_Settings.m_wLogComposedTypeToPack, LOG_COMPOSED_TYPE_TO_PACK);
-    ASSERT_EQ(g_Settings.m_bLogUnpackAll, LOG_UNPACK_ALL);
+    ASSERT_EQ(local_settings.m_wLogComposedTypeToPack, LOG_COMPOSED_TYPE_TO_PACK);
+    ASSERT_EQ(local_settings.m_bLogUnpackAll, LOG_UNPACK_ALL);
 
-    ASSERT_EQ(g_Settings.m_wSourceID, SOURCE_ID);
-    ASSERT_EQ(g_Settings.m_wStatusRequestMessageType, STATUS_REQUEST_MESSAGE_TYPE);
+    ASSERT_EQ(local_settings.m_wSourceID, SOURCE_ID);
+    ASSERT_EQ(local_settings.m_wStatusRequestMessageType, STATUS_REQUEST_MESSAGE_TYPE);
+
+    ASSERT_EQ(g_Settings.m_arStatusData[0], STATUS_DATA_0);
+    ASSERT_EQ(g_Settings.m_arStatusData[1], STATUS_DATA_1);
     //EXPECT_TRUE(true);
 }
 
